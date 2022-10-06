4875cda22db5ee3cc37cde4789c0032e4567f462
Christopher lau <chrislaustopher@berkeley.edu>
2022-10-05 18:32:27 -0700

    Add metadata to metrics view

--- NewRelic/MetricsScene/MetricsViewController.swift
+++ NewRelic/MetricsScene/MetricsViewController.swift
@@ -17,9 +17,12 @@ struct MetricsAttribute {
 public var responseValue = 0.0
 public var responseValueCount = 0.0
 
+
 class MetricsViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var metricData: [MetricsAttribute] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,12 +35,20 @@ class MetricsViewController: UIViewController {
         tableView.estimatedRowHeight = 140
         tableView.separatorStyle = .none
         tableView.bounces = false
+        let modelName = UIDevice.current.name
+        let os = UIDevice.current.systemVersion
+        
+        metricData.append(MetricsAttribute(label: "cat breed list (limit 30) avg response time",  value: String(format: "%f", responseValue / (responseValueCount == 0.0 ? 1.0 : responseValueCount))))
+        metricData.append(MetricsAttribute(label: "Device make", value: modelName))
+        metricData.append(MetricsAttribute(label: "OS Version", value: os))
     }
     
     func recalculate(newVal: Double) {
         responseValue = responseValue + newVal
         responseValueCount = responseValueCount + 1
     }
+    
+
 }
 
 extension MetricsViewController: UITableViewDelegate, UITableViewDataSource {
@@ -48,13 +59,13 @@ extension MetricsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
         // average response time, make/model, os version => 3
-        return 1
+        return 3
     }
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MetricsCell", for: indexPath) as? MetricsTableViewCell
-        cell?.configure(MetricsAttribute(label: "cat breed list (limit 30) avg response time", value: String(format: "%f", responseValue / (responseValueCount == 0.0 ? 1.0 : responseValueCount))))
+        cell?.configure(metricData[indexPath.row])
         return cell!
     }
 }
